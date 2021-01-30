ef67d992efe0f96840e16814fe669ebae8c2498d
Enrico Benini <benkio89@gmail.com>
2021-01-30 18:22:21 +0100

    - Add pretty much all the needed package. - Add the TODO of next things to setup

--- home.nix
+++ home.nix
@@ -2,7 +2,9 @@
 
 let
    packages = import <nixpkgs> {};
-   emacsConfig = packages.fetchgit {
+   # TODO: upgrade to the latest in here or do it after
+   #       from the emacs folder.
+   emacsConfig = packages.fetchgit { 
       url = "git://github.com/benkio/emacs-config.git";
       rev = "053ad1ded292bfa049c231d680c1c9530571aa37";
       sha256 = "0643pw2c0wsf45f8diw0yyfqz1vbdbixzjhkgqi4l3bak00zbvjy";
@@ -10,22 +12,10 @@ let
     };
 in 
 {
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+  # TODO: change those
   home.username = "nix";
   home.homeDirectory = "/home/nix";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "21.03";    
 
   home.sessionVariables = {
@@ -34,35 +24,53 @@ in
   };
 
   nixpkgs.config.allowUnfree = true;
-  home.packages = with pkgs; [
-ag
-aspell
-audacity
-autoconf
-awscli
-bleachbit
-calibre
-chromium
-curl
-docker
-ffmpeg
-filezilla
-firefox
-gimp
-htop
-jack2
-openjdk
-postgresql
-sbt
-scala
-tdesktop #telegra-desktop
-tldr
-transmission
-unrar
-vlc
-wget
-youtube-dl
-  ];
+  home.packages = with pkgs; [  
+    ag
+    aspell
+    audacity
+    autoconf
+    awscli
+    bleachbit
+    calibre
+    chromium
+    curl
+    dejavu_fonts
+    docker
+    ffmpeg
+    filezilla
+    firefox
+    gimp
+    gnupg
+    hexchat
+    htop
+    imagemagick
+    jack2
+    lilypond
+    nettools
+    nodejs
+    nodePackages.typescript
+    nodePackages.npm
+    pandoc
+    purescript
+    ntfs3g
+    obs-studio
+    jdk8
+    postgresql
+    pulseaudio #TODO: find out how to do .override { jackaudioSupport = true; }
+    qjackctl
+    sbt
+    scala
+    symbola
+    tdesktop #telegra-desktop
+    tldr
+    transmission
+    texlive.combined.scheme-medium
+    unrar
+    unzip
+    vlc
+    wget
+    youtube-dl
+    ];
 
   programs.emacs = {
     enable = true;
@@ -78,4 +86,12 @@ youtube-dl
     userName = "Enrico Benini";
     userEmail = "benkio89@gmail.com";
   };
+
+  # TODO: Haskell. No haskell platform??
+  # TODO: Create workspace folder with all the projects
+  # TODO: fstab
+  # TODO: i3 & xserver
+  # TODO: emacs deamon ??
+  # TODO: split/organize into separate files
+  
 }
