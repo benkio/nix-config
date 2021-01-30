3521f32fbe6b203aaf808a9b4368df6f9d089c43
Enrico Benini <benkio89@gmail.com>
2021-01-30 22:26:26 +0100

    commit from host machine :D
    Not able to let copy paste work on virtual box s#!t

--- home.nix
+++ home.nix
@@ -106,7 +106,14 @@ in
     windowManager.i3.enable = true;
   };
   home.file.".xinitrc".source = ''
-  
+   if [ -d /etc/X11/xinit/xinitrc.d ]; then
+  for f in /etc/X11/xinit/xinitrc.d/*; do
+    [ -x "$f" ] && . "$f"
+  done
+  unset f
+fi
+
+exec i3
   '';
 
   # TODO: i3 & xserver
