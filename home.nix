a42669bb116bda9097c6288e37e42db8e96f2c8b
Enrico Benini <benkio89@gmail.com>
2021-01-30 22:34:12 +0100

    Change source to text for xinitrc

--- home.nix
+++ home.nix
@@ -19,6 +19,8 @@ in
   home.homeDirectory = "/home/nix";
   home.stateVersion = "21.03";    
 
+  
+
   home.sessionVariables = {
     LANG = "en_US.UTF-8";
     EDITOR = "emacs";
@@ -105,15 +107,15 @@ in
     enable = true;
     windowManager.i3.enable = true;
   };
-  home.file.".xinitrc".source = ''
-   if [ -d /etc/X11/xinit/xinitrc.d ]; then
-  for f in /etc/X11/xinit/xinitrc.d/*; do
-    [ -x "$f" ] && . "$f"
-  done
-  unset f
-fi
+  home.file."xinitrc".text = ''
+    if [ -d /etc/X11/xinit/xinitrc.d ]; then
+      for f in /etc/X11/xinit/xinitrc.d/*; do
+        [ -x "$f" ] && . "$f"
+      done
+      unset f
+    fi
 
-exec i3
+    exec i3
   '';
 
   # TODO: i3 & xserver
