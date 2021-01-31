5f28f76e7e2258231c3f371828398a578e4c97ef
Enrico Benini <benkio89@gmail.com>
2021-01-31 12:58:10 -0500

    Minimal configuration of Xmonad
    Xmonad --recompile doesn't work!!! But online they say to reboot.
    I put it disable and reboot then retry

--- home.nix
+++ home.nix
@@ -122,37 +122,13 @@ in
     enable = true;
     windowManager.xmonad = {
       enable = true;
-      extraPackages = haskellPackages: [
-        haskellPackages.xmonad-contrib
-        haskellPackages.xmonad-extras
-        haskellPackages.xmonad
-      ];
+      enableContribAndExtras = true;
+      config = pkgs.writeText "xmonad.hs" ''
+        import XMonad
+        main = xmonad defaultConfig
+      '';
+
     };
-    config = pkgs.writeText "xmonad.hs" ''
-    import XMonad
-    import XMonad.Hooks.DynamicLog
-    import XMonad.Hooks.ManageDocks
-    import XMonad.Util.Run(spawnPipe)
-    import XMonad.Util.EZConfig(additionalKeys)
-    import System.IO
-
-    main = do
-        xmproc <- spawnPipe "xmobar"
-
-        xmonad $ defaultConfig
-            { manageHook = manageDocks <+> manageHook defaultConfig
-            , layoutHook = avoidStruts  $  layoutHook defaultConfig
-            , logHook = dynamicLogWithPP xmobarPP
-                            { ppOutput = hPutStrLn xmproc
-                            , ppTitle = xmobarColor "green" "" . shorten 50
-                            }
-            , modMask = mod4Mask     -- Rebind Mod to the Windows key
-            } `additionalKeys`
-            [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-            , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-            , ((0, xK_Print), spawn "scrot")
-            ]
-    '';
   };
 
   home.file.".ghci".text = ''
