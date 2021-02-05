45c217374e29319f50341c13721e2627684e9322
benkio <benkio89@gmail.com>
2021-02-05 20:29:14 +0100

    less install script

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # sudo apt-get install curl git
-echo "update nix channel"
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
-sudo  nix-channel --update
-echo "install home-manager"
-sudo nix-shell '<home-manager>' -A install
-nix-env --set-flag priority 0 home-manager
 echo "copy nix config"
 mkdir -p ~/.config/nixpkgs
 cp ./home.nix ~/.config/nixpkgs/
