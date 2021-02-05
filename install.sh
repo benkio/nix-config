9c403cdc0ef57c8f0191c1395fac64cd4472f3b6
benkio <benkio89@gmail.com>
2021-02-05 21:31:23 +0100

    delete install.sh, let's restart fresh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# sudo apt-get install curl git
-echo "copy nix config"
-mkdir -p ~/.config/nixpkgs
-cp ./home.nix ~/.config/nixpkgs/
-sudo cp ./configuration.nix /etc/nixos/configuration.nix
-echo "switch home manager"
-sudo nixos-rebuild switch
-sudo home-manager switch
