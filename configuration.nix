3b523e9ddc3a888e2b97736db038ac00254b07c2
benkio <benkio89@gmail.com>
2021-02-05 19:57:08 +0100

    import nixos

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,8 @@ imports =
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      # import home-manager
+      (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
       ];
 
   boot.loader.grub.enable = true;
