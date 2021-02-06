a962d72f2e30a2ef7f983a5b75a4d04e54554ede
Enrico Benini <benkio89@gmail.com>
2021-02-06 16:56:53 +0100

    Amule + i3 initial config

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
+  programs.dconf.enable = true;
+  
   imports =
     [
       # Include the results of the hardware scan.
@@ -59,12 +62,21 @@
         xfce.enable = true;
       };
       displayManager = {
-        defaultSession = "xfce";
+        defaultSession = "xfce+i3";
         lightdm.enable = true;
         autoLogin = {
           enable = true;
           user = "benkio";
         };
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu    # application launcher most people use
+          i3status # gives you the default i3 status bar
+          i3lock   # default i3 screen locker
+       ];
       };
     };
   };
--- home.nix
+++ home.nix
@@ -56,6 +56,7 @@ in
 
     packages = with pkgs; [
       ag
+      amule
       aspell
       audacity
       autoconf
