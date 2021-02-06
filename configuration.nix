e9c6dcf4ebe712276a47f2ec4f53425a18f1919d
Enrico Benini <benkio89@gmail.com>
2021-02-06 22:30:48 +0100

    remove unfree stuff. all compiles, reboot

--- configuration.nix
+++ configuration.nix
@@ -20,10 +20,7 @@ in {
   boot = {
     loader.grub.enable = true;
     loader.grub.version = 2;
-    loader.grub.device = "/dev/sda";
-    extraModulePackages = [
-      pkgs.linuxPackages.nvidia_x11
-    ]; 
+    loader.grub.device = "/dev/sda"; 
   };
 
   powerManagement.enable = true;
@@ -102,7 +99,6 @@ in {
      enableFontDir = true;
      enableGhostscriptFonts = true;
      fonts = with pkgs; [
-       vistafonts
        inconsolata
        terminus_font
        proggyfonts
