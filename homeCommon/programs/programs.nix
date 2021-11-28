f477f392a147c6381718094954546cc9a8ef28bd
Enrico Benini <benkio89@gmail.com>
2021-11-28 15:27:49 +0000

    git use delta

--- homeCommon/programs/programs.nix
+++ homeCommon/programs/programs.nix
@@ -21,6 +21,18 @@
       enable = true;
       userName = "Enrico Benini";
       userEmail = "benkio89@gmail.com";
+      delta = {
+        enable = true;
+        options = {
+          decorations = {
+            commit-decoration-style = "bold yellow box ul";
+            file-decoration-style = "none";
+            file-style = "bold yellow ul";
+          };
+          features = "decorations";
+          whitespace-error-style = "22 reverse";
+        };
+      };
     };
   };
 }
