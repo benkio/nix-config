78f2781c659ff71923eaff5ce9ae4b529b6fa87d
Enrico Benini <benkio89@gmail.com>
2023-01-16 14:55:34 +0000

    specify the version. Otherwise, you don't actually override it

--- MacOs/home/packages.nix
+++ MacOs/home/packages.nix
@@ -7,7 +7,7 @@
 
   nixpkgs.overlays = [ (self: super: {
     jdk11 = super.jdk11.overrideAttrs (oldAttrs:  {
-        #version = "11.0.17";
+        version = "11.0.17";
         src = super.fetchurl {
           url = "https://cdn.azul.com/zulu/bin/zulu11.60.19-ca-jdk11.0.17-macosx_x64.tar.gz ";
           sha256 = "sha256-8rL+gpKLmfArpg7U35iuOtZWQyNEFFX8l4dJinnA8mM=";
