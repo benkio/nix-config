e38f54fd6fac51073cff6ec64ed38e5c9ec2b280
Enrico Benini <benkio89@gmail.com>
2025-09-26 08:36:32 +0100

    🔄 `UpdateAllGitMain` 🔧⚠️ SSH Warning

--- homeCommon/gists.nix
+++ homeCommon/gists.nix
@@ -7,8 +7,8 @@
 
 let
   updateAllGitMain = pkgs.fetchurl {
-    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/7ad18c9b51f4f2d1f932186538d221f960dee0d9/UpdateAllGitMain.sc";
-    hash = "sha256-UkDgQRU629isEKUQBxABPt1FWXkH/QNRj6QkOcujJ68=";
+    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/bd1c25b350c6d4ff16462400c1553f4df92de0a8/UpdateAllGitMain.sc";
+    hash = "sha256-qax4YYnA1RvBazFXZzr8YgtjRzafXTP0lrMkXaOjUhI=";
   };
 in
 {
