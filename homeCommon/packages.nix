36b40312171aa3f1eea61ef2025442c0796e5b4c
Enrico Benini <benkio89@gmail.com>
2025-10-04 12:41:41 +0100

    ➕ `tsx`. Sort/Formatting lines

--- homeCommon/packages.nix
+++ homeCommon/packages.nix
@@ -19,25 +19,26 @@ let
 in
 {
   home.packages = with pkgs; [
-    # BROKEN handbrake
+                                            # BROKEN handbrake
     aspell                                  # Spelling checker
     aspellDicts.en                          # Spelling checker Dictionary
-    aspellDicts.en-science                  # Spelling checker Dictionary
     aspellDicts.en-computers                # Spelling checker Dictionary
+    aspellDicts.en-science                  # Spelling checker Dictionary
     aspellDicts.it                          # Spelling checker Dictionary
     cabal-install                           # Haskell package manager
     discord                                 # Chat
+    nodePackages.aws-cdk                    # AWS Cloud Development Kit
     nodePackages.js-beautify                # Javascript beautifier
-    nodePackages.npm                        # Node package manager
     nodePackages.jsonlint                   # Json Linter
+    nodePackages.npm                        # Node package manager
+    nodePackages.prettier                   # Javascript Formatter
     nodePackages.typescript                 # Typescript compiler
     nodePackages.typescript-language-server # Typescript LSP
     nodePackages.yarn                       # Package manager
-    nodePackages.prettier                   # Javascript Formatter
-    nodePackages.aws-cdk                    # AWS Cloud Development Kit
     sqlfluff                                # SQL Linter
-    tex
     symbola                                 # Font
+    tex                                     # Typesetting Engine
+    tsx                                     # Typescript Execute
     xorg.setxkbmap                          # Command to set keybindings
   ];
 }
