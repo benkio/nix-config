e4a5d72c417bf518c226945384c7ea9cad9fc841
Enrico Benini <benkio89@gmail.com>
2025-10-05 00:09:04 +0100

    previous commit

--- NixOs/home/sway.nix
+++ NixOs/home/sway.nix
@@ -109,7 +109,7 @@ in
           always = false;
         }
         {
-          command = "sleep 1; swww-daemon & sleep 1; while true; do swww img ${config.home.homeDirectory}/wallpapers/$(ls ${config.home.homeDirectory}/wallpapers | shuf -n 1) --transition-fps 30 --transition-type=random --transition-bezier=0,0.84,1,1; sleep 600; done";
+          command = "\"sleep 1; swww-daemon & sleep 1; while true; do swww img ${config.home.homeDirectory}/wallpapers/$(ls ${config.home.homeDirectory}/wallpapers | shuf -n 1) --transition-fps 30 --transition-type=random --transition-bezier=0,0.84,1,1; sleep 600; done\"";
           always = false;
         }
       ];
