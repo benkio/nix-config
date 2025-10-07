8729b0747df78209cec88b1b64aff97f51e94b32
Enrico Benini <benkio89@gmail.com>
2025-10-07 01:18:08 +0100

    ➕ `xdg` default applications

--- NixOs/home/home.nix
+++ NixOs/home/home.nix
@@ -44,4 +44,29 @@
       ESHELL = "~/.nix-profile/bin/bash";
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/x-extension-html"="floorp.desktop";
+      "application/x-extension-shtml"="floorp.desktop";
+      "application/x-extension-xht"="floorp.desktop";
+      "application/x-extension-xhtml"="floorp.desktop";
+      "application/xhtml+xml"="floorp.desktop";
+      "audio/*" = "vlc.desktop";
+      "audio/mpeg"="vlc.desktop";
+      "audio/webm"="vlc.desktop";
+      "text/html"="floorp.desktop";
+      "video/mp4"="vlc.desktop";
+      "video/mpeg"="vlc.desktop";
+      "video/webm"="vlc.desktop";
+      "video/x-msvideo"="vlc.desktop";
+      "x-scheme-handler/chrome"="floorp.desktop";
+      "x-scheme-handler/http"="floorp.desktop";
+      "x-scheme-handler/https"="floorp.desktop";
+      "x-scheme-handler/tg"="org.telegram.desktop.desktop";
+      "x-scheme-handler/tonsite"="org.telegram.desktop.desktop";
+      "application/x-extension-htm"="floorp.desktop";
+    };
+  };
 }
