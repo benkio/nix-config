71b6c713764c74d5acc414e14670210b9cfde883
Enrico Benini <benkio89@gmail.com>
2025-10-28 13:23:18 +0000

    🔄 gist version

--- homeCommon/gists.nix
+++ homeCommon/gists.nix
@@ -7,8 +7,8 @@
 
 let
   updateAllGitMain = pkgs.fetchurl {
-    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/bd1c25b350c6d4ff16462400c1553f4df92de0a8/UpdateAllGitMain.sc";
-    hash = "sha256-qax4YYnA1RvBazFXZzr8YgtjRzafXTP0lrMkXaOjUhI=";
+    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/1de7dfac1a283b7552b7146fda377ec51bc07deb/UpdateAllGitMain.sc";
+    hash = "sha256-2R62FubjIqjPERyq3m8whSkHiCLTU5nXB8gXsO2fnJU=";
   };
 in
 {
