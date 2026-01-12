c3456fe8afded77c05913ab47eaf669480ec44b8
Enrico Benini <benkio89@gmail.com>
2026-01-12 13:21:54 +0100

    Update nixos version

--- homeCommon/homeConfig.nix
+++ homeCommon/homeConfig.nix
@@ -23,7 +23,7 @@
       PGDATA = "${config.home.homeDirectory}/postgresDataDir";
       XDG_CURRENT_DESKTOP = "GNOME"; # To trick some app to work on i3 alone, eg gnome-control-center
     };
-    stateVersion = "25.05";
+    stateVersion = "25.11";
     username = "benkio";
     file = {
       ".ghci".text = ''
