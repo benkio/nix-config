a181aff1ffd041758b816f0aebbcd04cb813ee64
Enrico Benini <benkio89@gmail.com>
2026-02-17 20:19:35 +0000

    🔧 emacs installation

--- homeCommon/emacsConfig.nix
+++ homeCommon/emacsConfig.nix
@@ -5,10 +5,27 @@
   ...
 }:
 
+let
+  emacsPreludeInstallation = pkgs.writeShellApplication {
+    name = "emacs-prelude";
+    runtimeInputs = [
+      pkgs.git
+      pkgs.curl
+    ];
+    text = ''
+      if [ ! -d "${config.home.homeDirectory}/.emacs.d" ]; then
+	export PRELUDE_URL="https://github.com/benkio/prelude.git"
+	curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+      else
+	echo "emacs folder exists"
+      fi
+    '';
+  };
+in
 {
+  home.packages = [ emacsPreludeInstallation ];
+
   home.activation.linkEmacsConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    if [ ! -d "${config.home.homeDirectory}/.emacs.d" ] || [ ! "$(ls -A ${config.home.homeDirectory}/.emacs.d)" ]; then
-      export PRELUDE_URL="https://github.com/benkio/prelude.git" && curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
-    fi
+    ${emacsPreludeInstallation}/bin/emacs-prelude
   '';
 }
