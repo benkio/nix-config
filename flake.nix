a0289b7694228a6e1e6322422023603cfb60efe2
Enrico Benini <benkio89@gmail.com>
2026-04-02 12:10:36 +0100

    more checks on OS

--- flake.nix
+++ flake.nix
@@ -10,25 +10,43 @@
 
   outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs:
     let
-      # Detect host family in flake eval without importing Linux-specific modules on macOS.
-      isDarwin = builtins.pathExists "/System/Library/CoreServices/SystemVersion.plist";
+      # Set TARGET_SYSTEM in your environment to force non-darwin outputs (e.g. x86_64-linux).
+      detectionSystem = builtins.getEnv "TARGET_SYSTEM";
+      targetSystem =
+        if detectionSystem == "" then
+          "x86_64-darwin"
+        else
+          detectionSystem;
+      isDarwin = builtins.match ".*-darwin$" targetSystem != null;
 
       darwinSystems =
+        if isDarwin then
           import ./flake/hosts/darwin/default.nix {
             inherit inputs;
             system = "x86_64-darwin";
-          };
+          }
+        else
+          {};
       nixosSystems =
+        if isDarwin then
+          {}
+        else
           import ./flake/hosts/nixos/default.nix {
             inherit inputs;
             system = "x86_64-linux";
           };
       darwinHomeConfigurations =
+        if isDarwin then
           import ./flake/home/darwin.nix {
             inherit inputs;
             system = "x86_64-darwin";
-          };
+          }
+        else
+          {};
       nixosHomeConfigurations =
+        if isDarwin then
+          {}
+        else
           import ./flake/home/nixos.nix {
             inherit inputs;
             system = "x86_64-linux";
