73399e84ae0369521c9d7aa2d8fcb5e4d4a73e9d
Enrico Benini <benkio89@gmail.com>
2026-06-12 19:46:01 +0100

    Update latest `UpdateAllGitMain` script

--- homeCommon/gists.nix
+++ homeCommon/gists.nix
@@ -7,10 +7,10 @@
 
 let
   updateAllGitMain = pkgs.fetchurl {
-    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/ac179aa0902ba573fc2702241fda4efa35376228/UpdateAllGitMain.sc";
-    hash = "sha256-+L9DmaYTlYwCkfdzldRWvk/P1MYELc4n1voP2rWX7Qc=";
+    url = "https://gist.githubusercontent.com/benkio/dbb9523529c875b5472a89eb391df7b4/raw/bc79d128baac236b49c1c6a97d6722bbc6722549/UpdateAllGitMain.scala";
+    hash = "sha256-rrVEO5wEWLPy692X7GXwp8d8lsrNlYaQjH9Rq7teJVY=";
   };
 in
 {
-  home.file."UpdateAllGitMain.sc".source = "${updateAllGitMain}";
+  home.file."UpdateAllGitMain.scala".source = "${updateAllGitMain}";
 }
