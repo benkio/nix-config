e438ff378596a6c5f62d0b4a7a44153972d70a8b
Enrico Benini <benkio89@gmail.com>
2026-08-18 10:43:53 +0100

    Add aiSkills

--- homeCommon/aiSkills.nix
+++ homeCommon/aiSkills.nix
@@ -7,25 +7,25 @@ let
       owner = "VirtusLab";
       repo = "scala-skill";
       rev = "51eb40a55fa1c2a4c99f2980830d28bbea4a2ac7";
-      sha256 = "sha256-mTEQ01z8U+eYl6lI51pEyUrL7LQjvRmvuN8QrWVfV6w=";
+      sha256 = "sha256-HuI7Jg1xO6ulpAUo7ObF2LmBFEpLJORvwZflwQzEX+8=";
     };
     effectTsSkill = pkgs.fetchFromGitHub {
       owner = "tenequm";
       repo = "skills";
       rev = "a64cf91e8fad60fa231a2d484f7c021b100d3127";
-      sha256 = "sha256-Tce3uMRofDKRrp30qrvdRjqUJuyUYWJfthlzEgc2GNE=";
+      sha256 = "sha256-+8/lA6wD/FKgt7LEX9ixxzs6fux20JJQhPm1t5x3Dc4=";
     };
     fpTsSkill = pkgs.fetchFromGitHub {
       owner = "whatiskadudoing";
       repo = "fp-ts-skills";
       rev = "b1c9eb70fc48aec470a98b3436d5fda4b338df3c";
-      sha256 = "sha256-fuI2+RW44FomZxcDdlrdZ8QoQmwAc/pvY3M5Wi2S5eo=";
+      sha256 = "sha256-ge5T+/9hDK71LDlcrz8PaDsWfdRLdj+ZDchZIcovsPY=";
     };
     haskellSkill = pkgs.fetchFromGitHub {
       owner = "aiskillstore";
       repo = "marketplace";
       rev = "b1cc8fa5414840335cbed43082309fd32a013145";
-      sha256 = "sha256-ADiheJjveaEGWwoXcSHETVPWgkem56ra/Yhi8VGZ0FQ=";
+      sha256 = "sha256-N9JuN906SKHf2WaiGGdSbxx8K+g1rqgbZ6Codn2mEuw=";
     };
   };
 
